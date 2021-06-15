51a2f88b2f918c1ddbb419ea4ec79a573e235cd7
adjemaa <adjemaa@student.42.fr>
2021-06-15 16:11:21 +0200

    vector iterator done

--- Vector.hpp
+++ Vector.hpp
@@ -7,12 +7,69 @@ namespace ft
     class VectorIterator
     {
         public :
+        //keyword typedef is a type definition where's in "typedef X Y;" X defines Y so Y = X in syntax
             typedef T value_type;
             typedef ptrdiff_t difference_type;
             typedef T* pointer;
             typedef T& reference;
             typedef size_t  size_type;
-            
+        private :
+            typedef VectorIterator<T> It;
+            pointer p;
+            VectorIterator(pointer e) : p(e){}
+        public :
+            VectorIterator() : p(NULL) {}
+            VectorIterator(const It &elem) : p(elem.p) {}
+            ~VectorIterator() {}
+            It &operator=(const It &elem)
+            {
+                p = elem.p;
+                return *this;
+            }
+            It &operator++()
+            {
+                ++p;
+                return *this;
+            }
+            It operator++(int)
+            {
+                It it = *this;
+                ++p;
+                return it;
+            }
+            reference operator*() const
+            {
+                return *p;
+            }
+            pointer operator->() const
+		    {
+		    	return p;
+		    }
+            It &operator--()
+            {
+                --p;
+                return *this;
+            }
+            It operator--(int)
+            {
+                It it = *this;
+                --p;
+                return it;
+            }
+            It &operator+=(size_type diff)
+            {
+                p += diff;
+                return *this;
+            }
+            It &operator-=(size_type diff)
+            {
+                p -= diff;
+                return *this;
+            }
+            reference operator[](difference_type n) const
+		{
+			return *(*this + n);
+		}
     };
 }
 
