de13fa70cce9ad953cae0e77c5047c444bcb47e1
Adam JEMAA <adjemaa@e1r8p4.42.fr>
2021-06-23 00:25:46 +0200

    vector pretty much done needs debugging

--- Vector.hpp
+++ Vector.hpp
@@ -1,6 +1,8 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 #include <stdio.h>
+#include <memory>
+#include "Iterator.hpp"
 
 namespace ft
 {
@@ -103,7 +105,7 @@ namespace ft
         return (false);
     }
     template <typename T>
-	bool operator==(const VectorIterator<T> &one, const VectorIterator<T> &two)
+	bool operator!=(const VectorIterator<T> &one, const VectorIterator<T> &two)
     {
         return !(one == two);
     }
@@ -123,22 +125,23 @@ namespace ft
     VectorIterator<T> operator-(const VectorIterator<T> &it, size_t diff)
     {
         VectorIterator<T> tmp = it;
-        return it -= diff
+        return it -= diff;
     }
     template <typename T>
     VectorIterator<T> operator-(size_t diff, const VectorIterator<T> &it)
     {
-        return it -= diff
+        return it -= diff;
     }
 	template <typename T>
 	VectorIterator<T> operator+(const VectorIterator<T> &it, size_t diff)
 	{
-		VectorIterator<T> tmp = ite;
+		VectorIterator<T> tmp = it;
 		return tmp += diff;
 	}
     template <typename T>
 	VectorIterator<T> operator+(size_t diff, const VectorIterator<T> &it)
 	{
+        VectorIterator<T> tmp = it;
 		return tmp += diff;
 	}
     //https://www.cplusplus.com/reference/vector/vector/
@@ -155,37 +158,37 @@ namespace ft
             typedef const T &const_reference;
             typedef VectorIterator<T> iterator;
             typedef VectorIterator<const T> const_iterator;
-            typedef reverse_iterator<iterator> reverse_iterator;
-            typedef reverse_iterator<const iterator> const_reverse_iterator;
+            typedef ft::reverse_iterator<T> reverse_iterator;
+            typedef ft::reverse_iterator<const T> const_reverse_iterator;
         private :
             pointer array;
             size_type length;
-            size_type capacity;
+            size_type cap;
         public :
             //Constructs an empty container, with no elements.
-            Vector() : array(NULL), length(0), capacity(0) {}
+            Vector() : array(NULL), length(0), cap(0) {}
             //Constructs a container with n elements. Each element is a copy of val.
-            Vector(size_type n, const value_type& val = value_type()) : array(NULL), length(0), capacity(0)
+            Vector(size_type n, const value_type& val = value_type()) : array(NULL), length(0), cap(0)
             {
                 insert(begin(), n, val);
             }
             template <class InputIterator>
-            Vector (InputIterator first, InputIterator last) : array(NULL), length(0), capacity(0)
+            Vector (InputIterator first, InputIterator last) : array(NULL), length(0), cap(0)
             {
                 insert(begin(), first, last);
             }
-            Vector (const vector& x) : array(NULL), length(0), capacity(0)
+            Vector (const Vector& x) : array(NULL), length(0), cap(0)
             {
                 insert(begin(), x.begin(), x.end());
             }
             ~Vector()
             {
-                std::allocator<T> all;
-                for (int i = 0, i < length, i++)
+                std::allocator<value_type> all;
+                for (int i = 0; i < length; i++)
                     all.destroy(&array[i]);
-                all.deallocate(array, capacity);
+                all.deallocate(array, cap);
             }
-            Vector &operator=(const vector& x)
+            Vector &operator=(const Vector& x)
 		    {
                 erase(x.begin(), x.end());
                 insert(begin(), x.begin(), x.end());
@@ -209,19 +212,19 @@ namespace ft
             }
             reverse_iterator rbegin()
             {
-                return reverse_iterator<VectorIterator<T>>(end());
+                return reverse_iterator<VectorIterator<T> >(end());
             }
-            const_reverse_iterator rbegin()
+            const_reverse_iterator rbegin() const
             {
-                return reverse_iterator<VectorIterator<T>>(end());
+                return reverse_iterator<VectorIterator<T> >(end());
             }
             reverse_iterator rend()
             {
-                return reverse_iterator<VectorIterator<T>>(begin());
+                return reverse_iterator<VectorIterator<T> >(begin());
             }
-            const_reverse_iterator rend()
+            const_reverse_iterator rend() const
             {
-                return reverse_iterator<VectorIterator<T>>(begin());
+                return reverse_iterator<VectorIterator<T> >(begin());
             }
             size_type size() const
             {
@@ -240,7 +243,7 @@ namespace ft
             }
             size_type capacity() const
             {
-                return capacity;
+                return cap;
             }
             bool empty() const
             {
@@ -254,31 +257,35 @@ namespace ft
             {
                 if (n <= cap || cap == 0)
                     return ;
-                std::allocator all;
-                pointer copy = all::allocate(n) ;
-                for (int i = 0; i < len; i++)
+                std::allocator<T> all;
+                pointer copy = all.allocate(n) ;
+                for (int i = 0; i < length; i++)
                 {
-                    all::construct(&copy[i], array[i]);
-                    all:destory(&array[i]);
+                    all.construct(&copy[i], array[i]);
+                    all.destory(&array[i]);
                 }
-                all:deallocate(array, capacity);
+                all.deallocate(array, cap);
                 array = copy;
-                capacity = n;
+                cap = n;
             }
             reference operator[] (size_type n)
+            {
+                return (array[n]);
+            }
+            const_reference operator[] (size_type n) const
             {
                 return (array[n]);
             }
             reference at (size_type n)
             {
                 if (n >= size())
-                    throw std::out_of_range(ss.str());
+                    throw std::out_of_range("out of range\n");
                 return (array[n]);
             }
             const_reference at (size_type n) const
             {
                 if (n >= size())
-                    throw std::out_of_range(ss.str());
+                    throw std::out_of_range("out of range\n");
                 return (array[n]);
             }
             reference front()
@@ -321,12 +328,101 @@ namespace ft
                 insert(position, 1, val);
                 return (position);
             }
-            
+            template <class InputIterator>
+            void insert (iterator position, InputIterator first, InputIterator last)
+            {
+                while (first != last)
+                {
+                    insert(position, 1, *first);
+                    position++;
+                    ++first;
+                }
+                
+            }
+            void insert (iterator position, size_type n, const value_type& val)
+            {
+                if (!n)
+                    return;
+                reserve(length + n);
+                std::allocator<T> all;
+                for (size_type i = length - 1; i >= position - array; i--)
+                {
+                    all.construct(&array[i + n], array[i]);
+                    all.destroy(&array[i]);
+                }
+                for (size_type i = position - array; i < position - array + n; i++)
+                    all.construct(&array[i],val);
+                length = length + n;
+            }
+            iterator erase (iterator position)
+            {
+                erase(position, position + 1);
+            }
             iterator erase (iterator first, iterator last)
             {
-
+                std::allocator<T> all;
+                if (first <= last)
+                    return ;
+                for (size_type i = first - array; i < array + last; i++)
+                    all.destory(&array[i]);
+                insert(first, last, end());
+            }
+            void swap (Vector& x)
+            {
+                std::swap(array, x.array);
+                std::swap(length, x.length);
+                std::swap(cap, x.cap);
+            }
+            void clear()
+            {
+                erase(begin(), end());
             }
     };
+    //http://www.cplusplus.com/reference/vector/vector/operators/
+    //compares vectors including their content and size
+    template <typename T>
+    bool operator==(const Vector<T> &lhs, const Vector<T> &rhs)
+    {
+        size_t i = 0;
+        size_t j = 0;
+        if (lhs.size() != rhs.size())
+            return (false);
+        while (i < lhs.size() && j < rhs.size() && lhs[i] == rhs[j])
+        {
+            i++;
+            j++;
+        }
+        return (lhs[i] == rhs[j]);
+    }
+    template <typename T>
+    bool operator!=(const Vector<T> &lhs, const Vector<T> &rhs)
+    {
+        return (!(lhs == rhs));
+    }
+    template <typename T>
+    bool operator<(const Vector<T> &lhs, const Vector<T> &rhs)
+    {
+        return lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+    template <typename T>
+    bool operator>(const Vector<T> &lhs, const Vector<T> &rhs)
+    {
+        return (rhs < lhs);
+    }
+    template <typename T>
+    bool operator<=(const Vector<T> &lhs, const Vector<T> &rhs)
+    {
+        return (!(rhs < lhs));
+    }
+    template <typename T>
+    bool operator>=(const Vector<T> &lhs, const Vector<T> &rhs)
+    {
+        return (!(lhs < rhs));
+    }
+    template <typename T>
+    void swap (Vector<T>& x, Vector<T>& y)
+    {
+        x.swap(y);
+    }
 }
-
 #endif
